436abcf4df94c3176df19592f05b133016f4556a
miskens <miskens@hotmail.com>
2021-11-01 13:27:31 +0100

    new awesome stuph added

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ from datetime import datetime
 
 print("This is the time right now:")
 print(datetime.now())
+
+name = input("Write your name: ")
+age = input("Write your age: ")
+
+print("{} is {} years old.".format(name, age))
